2f0933015f43b896f1c47680655bff4dfd8c7dd1
Roshan <roshanbhuvad15@gmail.com>
2020-10-04 11:22:15 +0530

    build

---
+++ funds/build/precache-manifest.0e04f0b5e011d0e638e143e0659dbf0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535b8a27c19889838c627125154319ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681b05b3be3b818aac4b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "82b9d2ff4898d1cd54f9",
+    "url": "/static/css/main.e24dbcf4.chunk.css"
+  },
+  {
+    "revision": "681b05b3be3b818aac4b",
+    "url": "/static/js/2.651da1ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.651da1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b9d2ff4898d1cd54f9",
+    "url": "/static/js/main.65c303c2.chunk.js"
+  },
+  {
+    "revision": "9e7cad3a5599bcbd289b",
+    "url": "/static/js/runtime-main.e5c809d9.js"
+  }
+]);
